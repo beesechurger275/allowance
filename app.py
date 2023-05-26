fe0282956af9544a2496488619b2fa11a6c02ad2
John Doe <nlaky9@gmail.com>
2023-05-25 18:24:02 -0600

    code cleanup

--- app.py
+++ app.py
@@ -118,87 +118,88 @@ def transfer():
 
 
         return render_template("transfer.html", account=account)
-    
 
+    if 'username' in session:
+        username = session['username']
     else:
-        try:
-            username = session['username']
-        except KeyError:
-            return redirect(url_for('login'))
-        
-        if request.form["name"] == "" or request.form["amount"] == "":
-            flash("You need to fill in the form completely!")
-            return redirect(url_for("transfer"))
-        if float(request.form["amount"]) <= 0:
-            flash("Nice try. Enter a number above 0")
-            return redirect(url_for("transfer"))
-
-        this_db = db()
-        this_db.connect("data.sql")
-
-        query = f"""
-        SELECT id FROM users WHERE name='{sqlsafe(username)}';
-        """
-
-        q = this_db.readOne(query)
-        fromid = q[0]
-
-        balance = 0
-
-        q = f"""
-        SELECT SUM(transferamount) FROM transactions WHERE user_id_to='{fromid}';
-        """
-        h = this_db.read(q)
-        add = h[0][0]
-
-        q = f"""
-        SELECT SUM(transferamount) FROM transactions WHERE user_id_from='{fromid}';
-        """
-        h = this_db.read(q)
-        sub = h[0][0]
-
-        if add != None and sub != None:
-            balance = round(add - sub, 2)
-        elif add == None and sub == None:
-            flash("You can't transfer more than you have!")
-            return redirect(url_for("transfer"))
-        elif add != None and sub == None:
-            balance = round(add, 2)
-        elif add == None and sub != None:
-            balance = round(sub, 2)
-
-        if balance - float(request.form["amount"]) < 0:
-            flash("You can't transfer more than you have!")
-            return redirect(url_for("transfer"))
-
-        query = f"""
-        SELECT id FROM users WHERE name='{sqlsafe(request.form["name"])}'
-        """
-
-        q = this_db.readOne(query)
-        try:
-            toid = q[0]
-        except TypeError:
-            flash("Enter valid username!")
-            return redirect(url_for("transfer"))
-
-        if fromid == toid:
-            flash("You can't transfer to yourself!")
-            return redirect(url_for("transfer"))
-
-        query = f"""
-        INSERT INTO transactions (transferamount, user_id_to, user_id_from) VALUES ({sqlsafefloat(request.form["amount"])}, {toid}, {fromid}); 
-        """
-
-        this_db.execute(query)
-
-        this_db.close()
-
-        flash("Transfer complete!")
+        return redirect(url_for('login'))
+    
+    if request.form["name"] == "" or request.form["amount"] == "":
+        flash("You need to fill in the form completely!")
         return redirect(url_for("transfer"))
+    if float(request.form["amount"]) <= 0:
+        flash("Nice try. Enter a number above 0")
+        return redirect(url_for("transfer"))
+
+    this_db = db()
+    this_db.connect("data.sql")
+
+    query = f"""
+    SELECT id FROM users WHERE name='{sqlsafe(username)}';
+    """
+
+    q = this_db.readOne(query)
+    fromid = q[0]
+
+    balance = 0
+
+    q = f"""
+    SELECT SUM(transferamount) FROM transactions WHERE user_id_to='{fromid}';
+    """
+    h = this_db.read(q)
+    add = h[0][0]
+
+    q = f"""
+    SELECT SUM(transferamount) FROM transactions WHERE user_id_from='{fromid}';
+    """
+    h = this_db.read(q)
+    sub = h[0][0]
+
+    if add != None and sub != None:
+        balance = round(add - sub, 2)
+    elif add == None and sub == None:
+        flash("You can't transfer more than you have!")
+        return redirect(url_for("transfer"))
+    elif add != None and sub == None:
+        balance = round(add, 2)
+    elif add == None and sub != None:
+        balance = round(sub, 2)
+
+    if balance - float(request.form["amount"]) < 0:
+        flash("You can't transfer more than you have!")
+        return redirect(url_for("transfer"))
+
+    query = f"""
+    SELECT id FROM users WHERE name='{sqlsafe(request.form["name"])}'
+    """
+
+    q = this_db.readOne(query)
+    try:
+        toid = q[0]
+    except TypeError:
+        flash("Enter valid username!")
+        return redirect(url_for("transfer"))
+
+    if fromid == toid:
+        flash("You can't transfer to yourself!")
+        return redirect(url_for("transfer"))
+
+    query = f"""
+    INSERT INTO transactions (transferamount, user_id_to, user_id_from) VALUES ({sqlsafefloat(request.form["amount"])}, {toid}, {fromid}); 
+    """
+
+    this_db.execute(query)
+
+    this_db.close()
+
+    flash("Transfer complete!")
+    return redirect(url_for("transfer"))
 
 @app.route("/viewaccounts")
 def viewall():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+        
     this_db = db()
     this_db.connect("data.sql")
     query = f"""
@@ -240,7 +241,8 @@ def viewall():
 
 @app.route("/accounts/<name>") # TODO display: ID, Balance, Transaction History
 def individualaccount(name):
-
+    if 'username' not in session:
+        return redirect(url_for('login'))
     this_db = db()
     this_db.connect("data.sql")
 
@@ -323,6 +325,8 @@ def individualaccount(name):
 
 @app.route("/adminpanel/", methods=["GET", "POST"])
 def adminpanel():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     this_db = db()
     this_db.connect("data.sql")
 
@@ -369,52 +373,49 @@ def adminpanel():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        try:
-            test = session['username'] # try to cause a KeyError lmao
-            test = None # TODO maybe this is a bad way to do this...
-            return redirect(url_for("index"))
-        except KeyError:
+        if 'username' not in session:
             return render_template("login.html")
-        
-    elif request.method == "POST":
-        try:
-            test = session['username'] # pain
-            test = None
-            return redirect(url_for('index'))
-        except KeyError:
-            pass
-
-        this_db = db()
-        this_db.connect("data.sql")
-
-        if request.form['username'] == '' or request.form['password'] == '':
-            flash("Enter both username and password!")
-            this_db.close()
-            return redirect(url_for("login"))
-
-        query = f"""
-            SELECT hash FROM users WHERE name='{request.form['username']}';
-            """
-        try:
-            q = this_db.readOne(query)
-            hashCheck = q[0]
-            passHash = hashlib.sha256(bytes(request.form["password"], "utf-8")).hexdigest()
-        except TypeError:
-            flash("Username or password incorrect!")
-            this_db.close()
-            return redirect(url_for("login"))
-
-        if passHash != hashCheck:
-            flash("Username or password incorrect!")
-            this_db.close()
-            return redirect(url_for("login"))
-
-        session['username'] = request.form['username']
         return redirect(url_for("index"))
+        
+    try:
+        test = session['username'] # pain
+        test = None
+        return redirect(url_for('index'))
+    except KeyError:
+        pass
+
+    this_db = db()
+    this_db.connect("data.sql")
+
+    if request.form['username'] == '' or request.form['password'] == '':
+        flash("Enter both username and password!")
+        this_db.close()
+        return redirect(url_for("login"))
+
+    query = f"""
+        SELECT hash FROM users WHERE name='{request.form['username']}';
+        """
+    try:
+        q = this_db.readOne(query)
+        hashCheck = q[0]
+        passHash = hashlib.sha256(bytes(request.form["password"], "utf-8")).hexdigest()
+    except TypeError:
+        flash("Username or password incorrect!")
+        this_db.close()
+        return redirect(url_for("login"))
+
+    if passHash != hashCheck:
+        flash("Username or password incorrect!")
+        this_db.close()
+        return redirect(url_for("login"))
+
+    session['username'] = request.form['username']
+    return redirect(url_for("index"))
 
 
 @app.route("/addaccount", methods=['GET', 'POST'])
 def addaccount():
+    
 
     this_db = db()
     this_db.connect("data.sql")
@@ -477,12 +478,9 @@ def addaccount():
 @app.route("/account", methods=["GET", "POST"])
 def account():
     if request.method == "GET":
-        try:
-            test = session['username'] # try to cause a KeyError lmao
-            test = None
+        if 'username' in session:
             return render_template("account.html")
-        except KeyError:
-            return redirect(url_for("login"))
+        return redirect(url_for("login"))
     
     print(request.form["current"])
 
